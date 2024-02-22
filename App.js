4fde151954abda1c04e149f70170f6fcdbd91a4d
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-02-22 16:48:09 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={`flex-1 items-center justify-center bg-black`}>
+      <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-lg`}>
+      <Text style={tw`text-white text-3xl font-bold`}>Hello World</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
